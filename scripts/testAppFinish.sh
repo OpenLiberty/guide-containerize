3fc91951ae1ae6e0494f13951af4fc147ff5c7f4
Dylan Uhryniuk <dylan.uhryniuk@ibm.com>
2022-06-07 14:46:23 -0400

    Update superlinter, update README, add kernel-slim to testAppFinish

--- scripts/testAppFinish.sh
+++ scripts/testAppFinish.sh
@@ -15,9 +15,11 @@ mvn -Dhttp.keepAlive=false \
     -q clean package
     
 docker pull openliberty/open-liberty:kernel-java8-openj9-ubi
+docker pull icr.io/appcafe/open-liberty:full-java11-openj9-ubi
+docker pull icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi
 
-docker build -t system system/.
-docker build -t inventory inventory/.
+docker build -t system -f ./system/Dockerfile-full system/.
+docker build -t inventory -f ./inventory/Dockerfile-full inventory/.
 
 docker images -f "label=org.opencontainers.image.authors=Your Name" | grep system
 docker images -f "label=org.opencontainers.image.authors=Your Name" | grep inventory
@@ -56,6 +58,35 @@ then
 else
   echo inventory status:
   echo "$inventoryStatus"
+  echo ENDPOINT
+  exit 1
+fi
+
+docker stop inventory system
+docker rm inventory system
+
+docker build -t system system/.
+docker build -t inventory inventory/.
+
+docker images -f "label=org.opencontainers.image.authors=Your Name" | grep system
+docker images -f "label=org.opencontainers.image.authors=Your Name" | grep inventory
+
+docker run -d --name system -p 9080:9080 system
+docker run -d --name inventory -p 9081:9081 inventory
+
+sleep 120
+
+systemStatus="$(curl --write-out "%{http_code}\n" --silent --output /dev/null "http://localhost:9080/system/properties/")"
+inventoryStatus="$(curl --write-out "%{http_code}\n" --silent --output /dev/null "http://localhost:9081/inventory/systems/")"
+
+if [ "$systemStatus" == "200" ] && [ "$inventoryStatus" == "200" ]
+then
+  echo ENDPOINT OK
+else
+  echo inventory status:
+  echo "$inventoryStatus"
+  echo system status:
+  echo "$systemStatus"
   echo ENDPOINT
   exit 1
 fi
