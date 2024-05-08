900f4fae6295680e5da25aa9bdff15f925808da5
Gilbert Kwan <gkwan@ca.ibm.com>
2024-05-08 08:50:03 -0400

    Update dailyBuild.sh (#176)

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -4,12 +4,20 @@ do
     case "${flag}" in
         t) DATE="${OPTARG}";;
         d) DRIVER="${OPTARG}";;
+        b) BUILD="${OPTARG}";;
+        u) DOCKER_USERNAME="${OPTARG}";;
         *) echo "Invalid option";;
     esac
 done
 
 sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" ../start/system/pom.xml ../start/inventory/pom.xml ../finish/system/pom.xml ../finish/inventory/pom.xml
 cat ../start/system/pom.xml ../start/inventory/pom.xml ../finish/system/pom.xml ../finish/inventory/pom.xml
+
+if [[ "$DOCKER_USERNAME" != "" ]]; then
+    sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" system/Dockerfile inventory/Dockerfile
+    sed -i "s;RUN features.sh;;g" system/Dockerfile inventory/Dockerfile
+    cat system/Dockerfile inventory/Dockerfile
+fi
 
 ../scripts/testAppFinish.sh
 ../scripts/testAppStart.sh
